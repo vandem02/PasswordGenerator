c691a5c8f314e2a82147acdac9a8b0ec1653f0d2
Evan Vandenberg <vandem02@uwgb.edu>
2023-06-20 17:14:42 -0500

    Added javascript

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,32 @@ const numeric = document.getElementById('numeric')
 const special = document.getElementById('special')
 const errorElement = document.getElementById('errorElement')
 
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numericChars = "0123456789"
+var specialChars = "!?@#$%^&*"
+
+function createPassword() {
+    
+    var charsIncluded = ""
+    var password = ""
+
+    if (lowercase.checked) charsIncluded += lowercaseChars
+    if (uppercase.checked) charsIncluded += uppercaseChars
+    if (numeric.checked) charsIncluded += numericChars
+    if (special.checked) charsIncluded += specialChars
+
+    for (let i = 0; i < length.value; i++) {
+        password += charsIncluded[Math.floor(Math.random() * charsIncluded.length)]
+    }
+
+    alert(`Password:\n${password}`)
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(lowercase.checked || uppercase.checked || numeric.checked || special.checked);
+    if (lowercase.checked || uppercase.checked || numeric.checked || special.checked) {
+        errorElement.style.display = "none"
+        createPassword()
+    } else errorElement.style.display = "block"
 })
